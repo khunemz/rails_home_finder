8bd6c99660d16401cdaf0e5ccedb26e35afd5de8
Chutipong Roobklom <khunemz@gmail.com>
2022-07-20 15:13:11 +0700

    :beers: add flash message

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'dashboard/index'
+  # customization
+  # get 'dasboard#index' as :dashboard
+  get 'dashboard/properties'
   resources :properties
   devise_for :accounts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
